2b186bf3e833d1981f48c97e0c6b78521f19e70c
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-03-01 11:54:06 +0630

    initialize app

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+defineProps<{ msg: string }>();
+</script>
+
+<template>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
