107c2672091827bc01a0b74be415262bbb296b05
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-03-01 12:17:35 +0630

    setup tailwindcss

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-
 defineProps<{ msg: string }>();
 </script>
 
 <template>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <p class="text-blue-700 text-center mt-5">
+    Click on the Vite and Vue logos to learn more
+  </p>
 </template>
