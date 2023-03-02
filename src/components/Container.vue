e77ab7fcb8608a36e613ef8aa7fa38e007f9875b
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-03-02 09:57:08 +0630

    render component conditionally

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import { useMenuStore } from '../store';
 import { PersonalInfo, SelectPlan, PickAddOns, FinishingUp } from './form';
+
+const menuStore = useMenuStore();
+const { currentMenu } = storeToRefs(menuStore);
 </script>
 
 <template>
   <div class="flex-1 flex justify-center">
-    <PersonalInfo />
+    <PersonalInfo v-if="currentMenu === 1" />
+    <SelectPlan v-else-if="currentMenu === 2" />
+    <PickAddOns v-else-if="currentMenu === 3" />
+    <FinishingUp v-else-if="currentMenu === 4" />
   </div>
 </template>
